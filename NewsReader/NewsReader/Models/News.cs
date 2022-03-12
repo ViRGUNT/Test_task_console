f007172861a5398146cb6744a47ed4f8a07b1d92
ViTTOLD <ViTTOLD@JUPITER>
2022-03-12 14:46:54 +0300

    Creating News.cs

---
+++ NewsReader/NewsReader/Models/News.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq.Mapping;
+
+namespace NewsReader.Models
+{
+    [Table(Name = "News")]
+    public class News
+    {
+
+        [Column(IsPrimaryKey = true, IsDbGenerated = true, Name = "ID")]
+        public int ID { get; set; }
+
+        [Column(Name = "Publication_Date")]
+        public DateTime Publication_Date { get; set; }
+
+        [Column(Name = "Guid")]
+        public string Guid { get; set; }
+
+        [Column(Name = "SourseUrl")]
+        public string SourseUrl { get; set; }
+
+        [Column(Name = "SourceName")]
+        public string SourceName { get; set; }
+
+        [Column(Name = "News_title")]
+        public string News_title { get; set; }
+
+        [Column(Name = "News_description")]
+        public string News_description { get; set; }
+    }
+}
--- NewsReader/NewsReader/Program.cs
+++ NewsReader/NewsReader/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NewsReader.Models;
 
 namespace NewsReader
 {
@@ -10,6 +11,7 @@ namespace NewsReader
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
